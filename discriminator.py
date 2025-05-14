96f0cade9169877f472ee000e92586b4661cd1be
DEVA PRIYA DARSHINI CHEEPILLA <cheepilladevapriyadarshini@gmail.com>
2025-05-14 14:51:26 +0530

    Update discriminator.py

--- discriminator.py
+++ discriminator.py
@@ -4,23 +4,19 @@ import torch.nn as nn
 class Discriminator(nn.Module):
     def __init__(self, in_channels=3):
         super(Discriminator, self).__init__()
-
         def discriminator_block(in_filters, out_filters, normalize=True):
             layers = [nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)]
             if normalize:
                 layers.append(nn.BatchNorm2d(out_filters))
             layers.append(nn.LeakyReLU(0.2, inplace=True))
             return layers
-
         self.model = nn.Sequential(
             *discriminator_block(in_channels * 2, 64, normalize=False),
             *discriminator_block(64, 128),
             *discriminator_block(128, 256),
             *discriminator_block(256, 512),
-            nn.Conv2d(512, 1, 4, padding=1)  # Output 1 channel prediction map
+            nn.Conv2d(512, 1, 4, padding=1) 
         )
-
     def forward(self, input_img, target_img):
-        # Concatenate image and condition image by channels
         x = torch.cat((input_img, target_img), 1)
         return self.model(x)
